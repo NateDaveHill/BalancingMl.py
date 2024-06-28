a3ee687ef602a25db175826e771df2d6c72a48d8
NateDaveHill <nathanhill123@gmail.com>
2024-06-28 13:03:09 -0400

    buildling ML model and configuring the agent with the ML

--- BalancingMl.py
+++ BalancingMl.py
@@ -1,5 +1,6 @@
 import random
 import gym
+import numpy as np
 
 try:
     from tensorflow.keras.models import Sequential
@@ -17,13 +18,34 @@ except ImportError as e:
 
 
 
-env = gym.make("CartPole-v1", render_mode="human")
+env = gym.make("CartPole-v1")
 
 states = env.observation_space.shape[0]
 actions = env.action_space.n
 
-print(states)
-print(actions)
+model = Sequential()
+model.add(Flatten(input_shape=(1, states)))
+model.add(Dense(24, activation="relu"))
+model.add(Dense(24, activation="relu"))
+model.add(Dense(actions, activation="linear"))
+
+agent = DQNAgent(
+    model=model,
+    memory=SequentialMemory(limit=50000, window_length=1),
+    policy=BoltzmannQPolicy,
+    nb_actions=actions,
+    nb_steps_warmup=10,
+    target_model_update=0.01
+)
+
+agent.compile(Adam(lr=0.001), metrics=["mae"])
+agent.fit(env, nb_steps=100000, visualize=False, verbose=1)
+
+results = agent.test(env, nb_episodes=10, visualize=True)
+
+print(np.mea(results.history["episode_reward"]))
+
+env.close()
 
 # episodes = 10
 # for episode in range(1, episodes+1):
